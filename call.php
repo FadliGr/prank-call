f6745b5e3db408427b5f1afbaab15b761d7970ca
FadliGr <53651759+FadliGr@users.noreply.github.com>
2019-10-10 15:11:44 +0700

    Update call.php

--- call.php
+++ call.php
@@ -2,12 +2,12 @@
 // Limit 3x Telpon Setiap Satu Nomor
 function send($phone){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");   >
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, true);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");     >
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
 }
@@ -18,3 +18,8 @@ $nomor = trim(fgets(STDIN));
 $execute = send($nomor);
 print $execute;
 ?>
+
+
+
+
+
